667625aa01c35e5770c11f8881f0827223df4ffe
Jose Alves de Gouveia <gouveia32@gmail.com>
2021-05-26 21:37:20 -0300

    teste com Link

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,14 @@
+import Link from 'next/link';
+
 function Home(params) {
-    return <div>Home 2</div>
+    return (<div>
+            <h1>Home</h1>
+
+            <Link href="/sobre">
+                <a>Acessar a página Sobre</a>
+            </Link>
+
+        </div>)
 }
 
 export default Home
---
+++ pages/sobre.js
@@ -0,0 +1,14 @@
+import Link from 'next/link';
+
+function Sobre(params) {
+    return (<div>
+            <h1>Sobre</h1>
+
+            <Link href="/">
+                <a>Acessar a página Home</a>
+            </Link>
+
+        </div>)
+}
+
+export default Sobre
